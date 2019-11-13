1f5a9d148c283bb0e73b50efba110e67f49170c1
johnson <chengqiaosheng@gmail.com>
2019-11-13 18:44:45 +0800

    add testing for start span

--- opentracing_test.go
+++ opentracing_test.go
@@ -97,6 +97,98 @@ func TestClient(t *testing.T) {
 				t.Fatalf("Unexpected error starting server: %v", err)
 			}
 
+			ctx, span, err := StartSpan(tracer, "root")
+			assert.NoError(err)
+
+			req := c.NewRequest(serverName, "Test.Method", &TestRequest{IsError: tt.isError}, client.WithContentType("application/json"))
+			rsp := TestResponse{}
+			err = c.Call(ctx, req, &rsp)
+
+			if tt.isError {
+				assert.Error(err)
+			} else {
+				assert.NoError(err)
+			}
+			assert.Equal(rsp.Message, tt.message)
+
+			span.Finish()
+
+			spans := tracer.FinishedSpans()
+			assert.Len(spans, 3)
+
+			var rootSpan opentracing.Span
+			for _, s := range spans {
+				// order of traces in buffer is not garanteed
+				switch s.OperationName {
+				case "root":
+					rootSpan = s
+				}
+			}
+
+			for _, s := range spans {
+				assert.Equal(rootSpan.Context().(mocktracer.MockSpanContext).TraceID, s.Context().(mocktracer.MockSpanContext).TraceID)
+			}
+		})
+	}
+}
+
+func TestClientFromContext(t *testing.T) {
+	// setup
+	assert := assert.New(t)
+	for name, tt := range map[string]struct {
+		message     string
+		isError     bool
+		wantMessage string
+		wantStatus  string
+	}{
+		"OK": {
+			message:     "passed",
+			isError:     false,
+			wantMessage: "passed",
+			wantStatus:  "OK",
+		},
+		"Invalid": {
+			message:     "",
+			isError:     true,
+			wantMessage: "",
+			wantStatus:  "InvalidArgument",
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			tracer := mocktracer.New()
+
+			registry := memory.NewRegistry()
+			sel := selector.NewSelector(selector.Registry(registry))
+
+			serverName := "micro.server.name"
+			serverID := "id-1234567890"
+			serverVersion := "1.0.0"
+
+			c := cli.NewClient(
+				client.Selector(sel),
+				client.WrapCall(NewCallWrapper(tracer)),
+			)
+
+			s := srv.NewServer(
+				server.Name(serverName),
+				server.Version(serverVersion),
+				server.Id(serverID),
+				server.Registry(registry),
+				server.WrapSubscriber(NewSubscriberWrapper(tracer)),
+				server.WrapHandler(NewHandlerWrapper(tracer)),
+			)
+			defer s.Stop()
+
+			type Test struct {
+				*testHandler
+			}
+
+			s.Handle(s.NewHandler(&Test{new(testHandler)}))
+
+			if err := s.Start(); err != nil {
+				t.Fatalf("Unexpected error starting server: %v", err)
+			}
+
 			ctx, span, err := StartSpanFromContext(context.Background(), tracer, "root")
 			assert.NoError(err)
 
